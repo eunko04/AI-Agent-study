5d5180c3f9bb5d3a9520b0d0b3386f3ab5830e21
eunko04 <eunko04@gmail.com>
2025-09-16 12:05:55 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 from openai import OpenAI
-from dotenv import load_dotenv
 import os
 
-load_dotenv()
+openai_api_key = st.secrets.get("OPENAI_API_KEY") or os.getenv("OPENAI_API_KEY")
+client = OpenAI(api_key=api_key)
 
 with st.sidebar:
     openai_api_key = os.getenv('OPENAI_API_KEY')
